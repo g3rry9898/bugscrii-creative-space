cec691966a262846f8e43abc13dcbb9c578e4235
g3rry9898 <hydrogenbond36@gmail.com>
2025-04-22 23:12:13 +0300

    updated head tags

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-dec24956dfc1e61d.js","static/chunks/fd9d1056-10dfaec9b4318f4c.js","static/chunks/117-210a80346ba61023.js","static/chunks/main-app-661c6bb096de525c.js"],pages:{"/_app":["static/chunks/webpack-dec24956dfc1e61d.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-5603e117d31e9b54.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-dec24956dfc1e61d.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-5603e117d31e9b54.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
